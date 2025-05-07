2b7a18927a5414b0ae6ccaa64bfdc49224bf9cbe
Jeison1999 <ojeison21@gmail.com>
2025-05-07 10:21:11 -0500

    device funcionando con un unico correo

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,6 +15,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  devise_for :admins, skip: [:registrations]
+  get 'admin/dashboard', to: 'admin#dashboard', as: 'admin_dashboard'
+
+  
+
+
   
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
