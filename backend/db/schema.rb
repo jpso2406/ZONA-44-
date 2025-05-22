8757424dcab22de0a17d2ed67edca06db5dc7fa9
julianmeza33 <julianmezaandrestorres20@gmail.com>
2025-05-21 21:28:09 -0500

    listo

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_22_001811) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sub_groups", force: :cascade do |t|
+  create_table "subgrupos", force: :cascade do |t|
     t.string "name"
     t.integer "precio"
     t.string "descripcion"
@@ -68,12 +68,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_22_001811) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "subgrupos", force: :cascade do |t|
-    t.string "name"
-    t.integer "precio"
-    t.string "descripcion"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
