a8a4d44134f7ad9275f5765ba27f958050a6be97
Jeison ortiz <ojeison21@gmail.com>
2025-05-23 14:10:05 -0500

    Semilla creada (#98)
    Por medio de esta nueva caracteristica podran ingresar todos al panel del admin si tienen el correo y la contraseña.
    closed #97

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development?
+  admin_email = 'ojeison21@gmail.com'
+  admin_password = '1234567'
+
+  admin = Admin.find_or_initialize_by(email: admin_email)
+
+  unless admin.persisted?
+    admin.password = admin_password
+    admin.password_confirmation = admin_password
+    admin.confirmed_at = Time.now if admin.respond_to?(:confirmed_at)
+    admin.save!
+    puts "Admin creado: #{admin_email}"
+  else
+    puts "El admin ya existe, no se ha creado de nuevo: #{admin_email}"
+  end
+end
