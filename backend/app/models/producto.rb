c411862aa65c38d644ce062d94c1e25d7a810181
julianmeza33 <julianmezaandrestorres20@gmail.com>
2025-06-12 15:22:43 -0500

    relacion

--- backend/app/models/producto.rb
+++ backend/app/models/producto.rb
@@ -1,3 +1,4 @@
 class Producto < ApplicationRecord
+  belongs_to :grupo
     has_one_attached :foto
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_11_213339) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_12_194852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -66,6 +66,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_11_213339) do
     t.string "descripcion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "grupo_id"
+    t.index ["grupo_id"], name: "index_productos_on_grupo_id"
   end
 
   create_table "subgrupos", force: :cascade do |t|
@@ -78,4 +80,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_11_213339) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "productos", "grupos"
 end
