548b7a311b60da344a3475702852e6b97c165433
Jesuze06 <jesuszame09@gmail.com>
2025-06-25 17:29:50 -0500

    carrito funcionando

--- backend/app/controllers/carrito_controller.rb
+++ backend/app/controllers/carrito_controller.rb
@@ -1,16 +1,31 @@
 class CarritoController < ApplicationController
   def agregar
-    producto = params[:producto]
+    producto_id = params[:producto_id].to_s
 
-    # Inicializa el carrito si no existe
     session[:carrito] ||= []
-    session[:carrito] << producto
+    session[:carrito] << producto_id unless session[:carrito].include?(producto_id)
 
-    flash[:notice] = "#{producto.humanize} agregado al carrito."
+    producto = Producto.find_by(id: producto_id)
+    if producto
+      flash[:notice] = "#{producto.name} agregado al carrito."
+    else
+      flash[:alert] = "Producto no encontrado."
+    end
+
     redirect_to request.referer || root_path
   end
 
   def mostrar
-    @productos_en_carrito = session[:carrito] || []
+    ids = session[:carrito] || []
+    @productos_en_carrito = Producto.where(id: ids)
+  end
+
+  def eliminar
+    producto_id = params[:id].to_s
+    session[:carrito] ||= []
+    session[:carrito].delete(producto_id)
+
+    flash[:notice] = "Producto eliminado del carrito."
+    redirect_to mostrar_carrito_path
   end
 end
