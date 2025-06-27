f718185b9e169fb98615bf3cd5726e1904fcb6b3
Jesuze06 <jesuszame09@gmail.com>
2025-06-27 14:38:01 -0500

    Terminado

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -30,25 +30,27 @@ if Rails.env.development?
 
   imagenes_path = Rails.root.join("db", "seeds", "imagenes")
 
-
   # Grupos
   grupos_data = [
     { nombre: "Bebidas", slug: "bebidas", imagen: "bebida.jpg" },
     { nombre: "Salchipapa", slug: "salchipapa", imagen: "salchipapa.jpg" },
-    { nombre: "Perro", slug: "perro", imagen: "perroh.jpg" }
+    { nombre: "Perro", slug: "perro", imagen: "perroh.jpg" },
+    { nombre: "Hamburguesa", slug: "hamburguesa", imagen: "hamburguesa.jpeg" },
+    { nombre: "Picadas Mini", slug: "picadas-mini", imagen: "picadapolloycarne.jpeg" },
+    { nombre: "Sandwich", slug: "sandwich", imagen: "sandwichdepollo.jpeg" },
+    { nombre: "Asados", slug: "asados", imagen: "lonchadecerdo.jpeg" }
   ]
 
   grupos_data.each do |grupo_info|
-    grupo = Grupo.find_or_initialize_by(slug: grupo_info[:slug])
-    if grupo.new_record?
-      grupo.nombre = grupo_info[:nombre]
-      grupo.save!
+    grupo = Grupo.find_by(slug: grupo_info[:slug]) || Grupo.find_by(nombre: grupo_info[:nombre])
+
+    if grupo.nil?
+      grupo = Grupo.create!(nombre: grupo_info[:nombre], slug: grupo_info[:slug])
       puts "Grupo creado: #{grupo.nombre}"
     else
-      puts "El grupo ya existe: #{grupo.nombre}"
+      puts "Grupo ya existente: #{grupo.nombre}"
     end
 
-    # Adjuntar foto si no está y si hay imagen definida
     if grupo.respond_to?(:foto) && !grupo.foto.attached? && grupo_info[:imagen].present?
       ruta_imagen = imagenes_path.join(grupo_info[:imagen])
       if File.exist?(ruta_imagen)
@@ -62,8 +64,23 @@ if Rails.env.development?
 
   # Productos
   productos_data = [
-    { name: "salchipapa sensilla", precio: 8000, descripcion: "Salchipapa con salsa", grupo_slug: "salchipapa", imagen: "salchipapasencilla.jpg" },
-    { name: "perro sencillo", precio: 7000, descripcion: "Perro con papas", grupo_slug: "perro", imagen: "perrosencillo.jpg" }
+    { name: "salchipapa sencilla", precio: 14000, descripcion: "Salchipapa con salsa", grupo_slug: "salchipapa", imagen: "salchipapasencilla.jpg" },
+    { name: "El Sencillito Mozzarella", precio: 7000, descripcion: "Perro con papas", grupo_slug: "perro", imagen: "perrosencillo.jpg" },
+    { name: "El Sencillito a la Plancha", precio: 8000, descripcion: "Perro a la plancha", grupo_slug: "perro", imagen: "elsencilloalaplancha.jpeg" },
+    { name: "Zona44", precio: 23000, descripcion: "Salchicha Suiza, pollo, tocineta, maíz, bañada en salsa Alfredo y queso parmesano", grupo_slug: "perro", imagen: "perrozona44.jpeg" },
+    { name: "Chorri Perro", precio: 14000, descripcion: "Chorizo de cerdo", grupo_slug: "perro", imagen: "chorriperro.jpeg" },
+    { name: "Gemelo Mozzarella", precio: 12000, descripcion: "Perro con queso", grupo_slug: "perro", imagen: "gemelomozzarella.jpeg" },
+    { name: "Burguer Mixta", precio: 27000, descripcion: "150gr de la carne de la casa, 100gr de pollo, queso cheddar", grupo_slug: "hamburguesa", imagen: "burguermixta.jpeg" },
+    { name: "Big Burguer", precio: 27000, descripcion: "Doble carne de la casa (300gr) con queso cheddar", grupo_slug: "hamburguesa", imagen: "Hamburguesadoblecarne.jpeg" },
+    { name: "ChoriBurguer", precio: 27000, descripcion: "Carne de la casa 150 gr, queso americano, chorizo en chimichurri y cebolla caramelizada", grupo_slug: "hamburguesa", imagen: "hamburguesachoriburguer.jpeg" },
+    { name: "Hawaiana", precio: 22000, descripcion: "Carne de la casa 150 gr, piña en trozos, queso costeño asado", grupo_slug: "hamburguesa", imagen: "hamburguesahawaiana.jpeg" },
+    { name: "Picada Pollo y Carne", precio: 16000, descripcion: "Picada pollo y carne", grupo_slug: "picadas-mini", imagen: "picadapolloycarne.jpeg" },
+    { name: "Sandwich de Pollo", precio: 17000, descripcion: "sandwich de pollo", grupo_slug: "sandwich", imagen: "sandwichdepollo.jpeg" },
+    { name: "Loncha de Cerdo", precio: 24000, descripcion: "Loncha de cerdo con papas", grupo_slug: "asados", imagen: "lonchadecerdo.jpeg" },
+    { name: "Pechuga Asada", precio: 23000, descripcion: "Pechuga 300gr con papas", grupo_slug: "asados", imagen: "pechugaasada.jpeg" },
+    { name: "Costillas BBQ", precio: 30000, descripcion: "500gr con papas", grupo_slug: "asados", imagen: "costillasbbq.jpeg" },
+    { name: "Limonada Frappe Cerezada", precio: 6000, descripcion: "Limonada Cerezada", grupo_slug: "bebidas", imagen: "limonadafrappecerezada.jpeg" },
+    { name: "Cocacola Personal", precio: 4000, descripcion: "Bebida Personal", grupo_slug: "bebidas", imagen: "cocacola.jpg" },
   ]
 
   productos_data.each do |producto_info|
@@ -74,17 +91,13 @@ if Rails.env.development?
     end
 
     producto = Producto.find_or_initialize_by(name: producto_info[:name], grupo_id: grupo.id)
-    if producto.new_record?
-      producto.precio = producto_info[:precio]
-      producto.descripcion = producto_info[:descripcion]
-      producto.grupo = grupo
-      producto.save!
-      puts "Producto creado: #{producto.name}"
-    else
-      puts "El producto ya existe: #{producto.name}"
-    end
 
-    # Adjuntar foto si no está y si hay imagen definida
+    producto.precio = producto_info[:precio]
+    producto.descripcion = producto_info[:descripcion]
+    producto.grupo = grupo
+    producto.save!
+    puts "Producto creado o actualizado: #{producto.name}"
+
     if producto.respond_to?(:foto) && !producto.foto.attached? && producto_info[:imagen].present?
       ruta_imagen = imagenes_path.join(producto_info[:imagen])
       if File.exist?(ruta_imagen)
