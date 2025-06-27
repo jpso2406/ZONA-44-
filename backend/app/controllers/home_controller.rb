5089f2a54c6c1b45d6bd27bc2f2d9d5035ed5004
juan pablo <168228566+jpso2406@users.noreply.github.com>
2025-06-27 10:44:53 -0500

    Feature/169 (#180)
    * terminado el diseño de menu y del carrito
    
    * eliminar el boton de agregar al carrito
    
    * terminado

--- backend/app/controllers/home_controller.rb
+++ backend/app/controllers/home_controller.rb
@@ -2,9 +2,18 @@ class HomeController < ApplicationController
   def bienvenidos
   end
 
- def menu
+  def menu
     @grupos = Grupo.all
+    @productos_en_carrito = productos_en_carrito
   end
+
   def contacto
+  end
+
+  private
+
+  def productos_en_carrito
+    ids = session[:carrito] || []
+    Producto.where(id: ids)
   end
 end
