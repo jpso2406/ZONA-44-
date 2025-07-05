967be20e57ea34ef4c39cd565f43cfb2d28fbace
Santiago Filigrama J. <filigramasantiago@gmail.com>
2025-07-05 12:52:23 -0500

    lista la migración

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_154950) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_05_174858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_154950) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "adicionals", force: :cascade do |t|
+    t.string "ingredientes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "admins", force: :cascade do |t|
