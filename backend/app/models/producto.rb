48e2c1cad58acd549fa366bd5c541938ff2a0ede
julianmeza33 <julianmezaandrestorres20@gmail.com>
2025-07-11 15:19:34 -0500

    listo

--- backend/app/models/producto.rb
+++ backend/app/models/producto.rb
@@ -5,12 +5,13 @@ class Producto < ApplicationRecord
 
   has_many :producto_adicionales, class_name: "ProductoAdicional"
   has_many :adicionales, through: :producto_adicionales, source: :adicional
-  def adicional_ids
-  adicionales.pluck(:id)
-end
 
-def adicional_ids=(ids)
-  self.adicionales = Adicional.where(id: ids.reject(&:blank?))
-end
+  def adicional_ids
+    adicionales.pluck(:id)
+  end
+
+  def adicional_ids=(ids)
+    self.adicionales = Adicional.where(id: ids.reject(&:blank?))
+  end
 end
 
