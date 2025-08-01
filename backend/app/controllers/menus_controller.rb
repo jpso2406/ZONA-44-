e004f0207efd39b80bccb81602a195075a5012aa
Jesuze06 <jesuszame09@gmail.com>
2025-07-31 22:17:27 -0500

    Links y rutas actualizados

--- backend/app/controllers/menus_controller.rb
+++ backend/app/controllers/menus_controller.rb
@@ -1,13 +1,17 @@
-class MenusController < ApplicationController
-def grupo
-  @grupo = Grupo.find(params[:id])
-  @productos = @grupo.productos
+class MenusController < ApplicationController 
 
-  # Si el slug en la URL no coincide, redirige al correcto
-  if params[:slug] != @grupo.slug
-    return redirect_to menu_grupo_path(@grupo.id, @grupo.slug), status: :moved_permanently
+  def grupo
+    @grupo = Grupo.find(params[:id])
+    @productos = @grupo.productos
+
+    # Redirección si el slug no coincide
+    if params[:slug] != @grupo.slug
+      return redirect_to menu_grupo_path(@grupo.id, @grupo.slug), status: :moved_permanently
+    end
   end
-end
 
+  def general
+    @grupos = Grupo.all.includes(:productos)
+  end
 
 end
