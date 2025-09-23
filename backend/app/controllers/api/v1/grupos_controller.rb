a7a3ba5eb0f842fdccb79353d38a6189f7909b87
Jeison1999 <ojeison21@gmail.com>
2025-09-22 21:25:01 -0500

    listo

--- backend/app/controllers/api/v1/grupos_controller.rb
+++ backend/app/controllers/api/v1/grupos_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class GruposController < ApplicationController
+      skip_before_action :authenticate_user!
       def index
         grupos = Grupo.includes(productos: [foto_attachment: :blob])
         render json: grupos.as_json(
